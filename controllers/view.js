873d2823deca43f445f47fe9367a230d1ffcecc5
Vilma Qerama <vilma.qerama@hotmail.com>
2021-07-13 09:53:11 +0100

    add view routes

--- controllers/view.js
+++ controllers/view.js
@@ -1,11 +1,11 @@
-const Router = require("express").Router();
+const router = require("express").Router();
 const path = require("path");
 
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
-router.get("/stats", (req,res)=> {
-  res.sendFile(path.join(__dirname, "../public/stats.html"))
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
-module.exports = Router
+module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const apiRoutes = require("./controllers/api");
+const app = express();
+// const apiRoutes = require("./controllers/api");
 const viewRoutes = require("./controllers/view");
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-
 app.use(logger("dev"));
+// app.use("/api",apiRoutes);
+app.use(viewRoutes);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,8 +22,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-app.use(apiRoutes);
-app.use(viewRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
